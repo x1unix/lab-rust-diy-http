0e61f90a080eeceb254d92bcb73761def41ad3ad
denys.sedchenko <denys.sedchenko@vestiairecollective.com>
2022-06-27 22:42:21 +0200

    L51: add response struct

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -2,10 +2,12 @@
 pub use request::{Method, ParseError, Request};
 pub use server::Server;
 pub use query_string::{QueryParam, QueryString};
+pub use response::*;
 
 // import * as server from './server';
 mod server;
 mod query_string;
+mod response;
 
 // export * as request from './request';
 pub mod request;
