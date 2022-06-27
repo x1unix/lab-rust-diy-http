6cf42522c7cdaf9a24c8ed2c00cc24166886065d
x1unix <denis0051@gmail.com>
2022-06-27 02:43:06 +0200

    L43: Parse method

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,6 +1,8 @@
+use core::panic;
 use std::convert::{From, TryFrom};
 use std::error::Error;
 use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
+use std::str::FromStr;
 
 pub enum Method {
     GET,
@@ -14,6 +16,27 @@ pub enum Method {
     PATCH,
 }
 
+pub struct InvalidMethod;
+
+impl FromStr for Method {
+    type Err = InvalidMethod;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "GET" => Ok(Method::GET),
+            "POST" => Ok(Method::POST),
+            "PUT" => Ok(Method::PUT),
+            "HEAD" => Ok(Method::HEAD),
+            "DELETE" => Ok(Method::DELETE),
+            "OPTIONS" => Ok(Method::OPTIONS),
+            "CONNECT" => Ok(Method::CONNECT),
+            "TRACE" => Ok(Method::TRACE),
+            "PATCH" => Ok(Method::PATCH),
+            _ => Err(InvalidMethod),
+        }
+    }
+}
+
 pub struct Request {
     path: String,
     method: Method,
@@ -23,9 +46,32 @@ pub struct Request {
 impl TryFrom<&[u8]> for Request {
     type Error = ParseError;
     fn try_from(buff: &[u8]) -> Result<Self, Self::Error> {
-        let data = std::str::from_utf8(buff)?;
+        let req_str = std::str::from_utf8(buff)?;
+        let (method, req_str) = get_next_word(req_str).ok_or(ParseError::InvalidRequest)?;
+        let (path, req_str) = get_next_word(req_str).ok_or(ParseError::InvalidRequest)?;
+        let (protocol, _) = get_next_word(req_str).ok_or(ParseError::InvalidRequest)?;
+
+        if protocol != "HTTP/1.1" {
+            return Err(ParseError::InvalidProtocol);
+        }
+
+        let method: Method = method.parse()?;
+
         unimplemented!();
     }
+}
+
+fn get_next_word(src: &str) -> Option<(&str, &str)> {
+    if src.is_empty() {
+        return None;
+    }
+    for (i, char) in src.chars().enumerate() {
+        if char.is_whitespace() {
+            return Some((&src[..i], &src[i + 1..]));
+        }
+    }
+
+    None
 }
 
 pub enum ParseError {
@@ -43,6 +89,12 @@ impl ParseError {
             Self::InvalidProtocol => "Invalid protocol",
             Self::InvalidEncoding => "Invalid encoding",
         }
+    }
+}
+
+impl From<InvalidMethod> for ParseError {
+    fn from(_: InvalidMethod) -> Self {
+        Self::InvalidMethod
     }
 }
 
