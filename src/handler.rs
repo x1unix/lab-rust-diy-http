aa2ed23750309e42e65653d9c7d1c0fc7a438427
denys.sedchenko <denys.sedchenko@vestiairecollective.com>
2022-06-28 02:26:11 +0200

    L59: serve files from public dir

--- src/handler.rs
+++ src/handler.rs
@@ -1,10 +1,24 @@
 use crate::http::{Handler, ParseError, Request, Response, StatusCode, Method};
+use std::fs;
 
-pub struct EchoHandler {}
+pub struct EchoHandler {
+    static_dir: String
+}
 
 impl EchoHandler {
-    pub fn new() -> Self {
-        Self {}
+    pub fn new(static_dir: String) -> Self {
+        Self { static_dir }
+    }
+    fn read_file(&self, path: &str) -> std::io::Result<String> {
+        // TODO: use filepath join and check path
+        let full_path = format!("{}/{}", self.static_dir, path);
+        fs::read_to_string(full_path.as_str())
+    }
+    fn serve_file(&self, path: &str) -> Response {
+        match self.read_file(path) {
+            Ok(data) => Response::new(StatusCode::OK, Some(data)),
+            Err(err) => Response::new(StatusCode::NotFound, Some(format!("Error: {err}")))
+        }
     }
 }
 
@@ -12,29 +26,8 @@ impl Handler for EchoHandler {
     fn handle_request(&mut self, req: &Request) -> Response {
         match req.method() {
             Method::GET => match req.path() {
-                "/" => {
-                    let body = format!(
-                        "<!DOCTYPE html>
-                        <html>
-                            <head>
-                                <title>&#129408; &#129408; &#129408;</title>
-                            </head>
-                            <body>
-                                <h1>Hello from &#129408;!</h1>
-                                <div>
-                                    <p>Your request URL:</p>
-                                    <pre>{} {}</pre>
-                                </div>
-                                <a href=\"/admin\">Admin panel</a>
-                            </body>
-                        </html>",
-                        req.method(), req.path()
-                    );
-            
-                    Response::new(StatusCode::OK, Some(body))
-                },
-                "/admin" => Response::new(StatusCode::Unauthorized, Some("Access Denied".to_string())),
-                _ => Response::new(StatusCode::NotFound, Some(format!("Page not found - {}", req.path())))
+                "/" => self.serve_file("index.html"),
+                path => self.serve_file(path),
             },
             _ => Response::new(StatusCode::BadRequest, Some("Unsupported HTTP method".to_string()))
         }
--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,17 @@
 
 mod handler;
 mod http;
+use std::env;
+
 use http::Server;
 
 fn main() {
-    let mut handler = handler::EchoHandler::new();
+    // Use build directory as root if public dir is not defined
+    let static_path = env::var("PUBLIC_DIR")
+        .unwrap_or(format!("{}/public", env!("CARGO_MANIFEST_DIR")).to_string());
+
+    println!("Serving files from {}", static_path);
+    let mut handler = handler::EchoHandler::new(static_path);
     let srv = Server::new("127.0.0.1:8080".to_string());
     srv.start(&mut handler);
 }
